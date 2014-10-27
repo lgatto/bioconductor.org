a7e8129273e48a00a460b8816062cd9c15642136
aoles <andrzej.oles@gmail.com>
2014-10-27 18:11:47 +0100

    Code cleanup

--- lib/data_sources/pubmed.rb
+++ lib/data_sources/pubmed.rb
@@ -1,14 +1,7 @@
 # encoding: utf-8
-
-require 'rubygems'
-require 'httparty' # OpenSSL
-require 'time' # Date.time
-require 'yaml'
-require 'fileutils'
-require 'nokogiri' # Nokogiri::HTML
-require 'open-uri' # open
+require 'nokogiri'
+require 'open-uri'
 require 'nanoc3'
-require 'nanoc'
 
 class PubmedPapers < Nanoc3::DataSource
     identifier :pubmed_papers
@@ -32,15 +25,14 @@ class PubmedPapers < Nanoc3::DataSource
 	db = "pubmed"
 	retmax = 20
 	
-	## search query
-        search = "#{baseurl}esearch.fcgi?db=#{db}&term=bioconductor&retmax=#{retmax}&usehistory=y"
+	## search
+        search = "#{baseurl}esearch.fcgi?db=#{db}&term=bioconductor&retmax=#{retmax}"
         doc = nil
         begin
             doc = Nokogiri::XML(open(search))
         rescue
             return []
         end
-	#webenv = doc.xpath("//eSearchResult/WebEnv").text
         idl = doc.xpath("/eSearchResult/IdList/Id")
 	return [] if idl.empty?
 	
@@ -81,64 +73,6 @@ class PubmedPapers < Nanoc3::DataSource
 	ret
     end
     
-    def fetchOld
-        baseurl = "http://www.ncbi.nlm.nih.gov/pubmed"
-	dispmax = 3
-        query = "#{baseurl}?term=bioconductor&dispmax=#{dispmax}"
-	
-	
-        # TODO ensure url exists?
-        # why is the certificate not valid from dan's home laptop?
-        doc = nil
-        begin
-            doc = Nokogiri::HTML(open(query))
-        rescue
-            return []
-        end
-        results = doc.css(".rslt")
-        return [] if results.empty?
-        
-	results.each do |result|
-	    a = result.css(".title a")
-	    href = a.attr('href').value
-	    title = a.text
-	    author = result.css(".desc").text
-	    journal = result.css(".jrnl").text
-	    details = result.css(".details").text
-	    details = details[journal.length+2..details.length-1]
-	      
-            next unless i % 2 == 0
-            x = a[i]
-            href = x.attr('href')
-            subject = x.text.strip.sub(/^\[Bioc-devel\] /, "")
-            msg_url = url.sub("date.html", href)
-            msg_doc = nil
-            begin
-                msg_doc = Nokogiri::HTML(open(msg_url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}))
-            rescue
-                return []
-            end
-            datestamp = msg_doc.css("i").first.text#.gsub(/[ ]{2,}/," ")
-            datestamp.sub!("CEST", "+0200")
-            datestamp.sub!("CET", "+0100")
-            dt = Time.strptime(datestamp, "%a %b %e %H:%M:%S %z %Y")
-            isodate = dt
-            #isodate = Time.parse(datestamp)#.utc#.iso8601
-            attributes = {
-                :title => subject,
-                :date => isodate,
-                :link => msg_url,
-                :author => "unused"
-            }
-            content = "unused"
-            identifier = "/#{href.gsub(/\.html/, "")}/"
-            mtime = nil
-            ret.push Nanoc3::Item.new(content, attributes, identifier, mtime)
-        end
-        ret.reverse
-    end
-
-
     def items
         fetch()
     end
